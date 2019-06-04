9ff50284ae1487e1124df5e722d1d20e63c52038
Stefania Giannopoulou <stefania.gian91@gmail.com>
2019-06-04 09:22:25 +0300

    first commit

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello{
+	
+	public static void main(String[]args){
+	//This is an one-line comment	
+	/*This is a multi-line comment
+	  continues...
+	  continues...
+	  continues....
+	*/
+	String name = "Stefania";
+	int age = 28;
+	int year = 2019;	
+		System.out.println("Hello " + name + " ,your age is: " + age);
+		System.out.println();
+		System.out.println("The year is " + year);
+		System.out.print(age);
+		System.out.print(name);
+		
+	}
+}
